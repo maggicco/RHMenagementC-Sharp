21007515587c924e48cd701640a0c096e0826cc2
kiszczak piotr <maggicco83@hotmail.fr>
2019-09-10 09:29:59 +0200

    Added login and chart works

---
+++ RHMenagment/LoginWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using FirstFloor.ModernUI.Windows.Controls;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace RHMenagment
+{
+    /// <summary>
+    /// Interaction logic for LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        SqlConnection sqlConnection;
+
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnSubmit_Click(object sender, RoutedEventArgs e)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["RHMenagment.Properties.Settings.RhMenagementConnectionString"].ConnectionString;
+            sqlConnection = new SqlConnection(connectionString);
+
+            try
+            {
+                if (sqlConnection.State == ConnectionState.Closed)
+                    sqlConnection.Open();
+                String query = "SELECT COUNT(1) FROM [User] WHERE UserName = @Username AND Password = @Password";
+                SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
+                sqlCommand.CommandType = CommandType.Text;
+
+                sqlCommand.Parameters.AddWithValue("@Username", txtUsername.Text);
+                sqlCommand.Parameters.AddWithValue("@Password", txtPassword.Password);
+
+                int count = Convert.ToInt32(sqlCommand.ExecuteScalar());
+                if (count == 1)
+                {
+                    MainWindow dashboard = new MainWindow();
+                    dashboard.Show();
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Mdp ou UserName incorrect !");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                sqlConnection.Close();
+            }
+        }
+
+        private void MyButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ RHMenagment/Pages/Absence.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using LiveCharts;
+using LiveCharts.Wpf;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RHMenagment.Pages
+{
+    /// <summary>
+    /// Interaction logic for Absence.xaml
+    /// </summary>
+    public partial class Absence : UserControl
+    {
+        public Absence()
+        {
+            InitializeComponent();
+
+            SeriesCollection = new SeriesCollection
+            {
+                new LineSeries
+                {
+                    Title = "Anée 2018",
+                    Values = new ChartValues<double> {5,1,5,3,7,4,1, 4, 6, 5, 2 ,4 }
+                },
+                new LineSeries
+                {
+                    Title = "Anée 2019",
+                    Values = new ChartValues<double> { 2,4,8,1,3,1,2, 6, 7, 3, 4 ,6 }
+                    
+                }
+
+            };
+
+            Labels = new[] { "January", "February", "Mars", "Avril", "Mai", "Juin", "Juillet", "Aout", "Septembre", "Octobre", "Novembre", "Decembre" };
+            YFormatter = value => value.ToString();
+
+
+            DataContext = this;
+        }
+
+        public SeriesCollection SeriesCollection { get; set; }
+        public string[] Labels { get; set; }
+        public Func<double, string> YFormatter { get; set; }
+    }
+}
